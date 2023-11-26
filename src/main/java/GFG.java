81a8f9092b7cc6a663979433c24818c19039cf61
Dheeraj Sehrawat <dheeraj.sehrawat@nexthink.com>
2023-11-26 22:11:04 +0530

    more weka example

---
+++ src/main/java/GFG.java
@@ -0,0 +1,69 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.Random;
+import weka.classifiers.Evaluation;
+import weka.classifiers.trees.J48;
+import weka.core.DenseInstance;
+import weka.core.Instances;
+
+// Main class
+// BreastCancer
+public class GFG {
+
+    // Main driver method
+    public static void main(String args[])
+    {
+
+        // Try block to check for exceptions
+        try {
+
+            // Create J48 classifier by
+            // creating object of J48 class
+            J48 j48Classifier = new J48();
+
+            // Dataset path
+            String breastCancerDataset
+                    = "breast-cancer.arff";
+
+            // Creating bufferedreader to read the dataset
+            BufferedReader bufferedReader
+                    = new BufferedReader(
+                    new FileReader(breastCancerDataset));
+
+            // Create dataset instances
+            Instances datasetInstances
+                    = new Instances(bufferedReader);
+
+            // Set Target Class
+            datasetInstances.setClassIndex(
+                    datasetInstances.numAttributes() - 1);
+
+            // Evaluating by creating object of Evaluation
+            // class
+            Evaluation evaluation
+                    = new Evaluation(datasetInstances);
+            j48Classifier.buildClassifier(datasetInstances);
+
+            // Cross Validate Model with 10 folds
+            evaluation.crossValidateModel(
+                    j48Classifier, datasetInstances, 10,
+                    new Random(1));
+
+            System.out.println(evaluation.toSummaryString(
+                    "\nResults", false));
+
+            for(int i=0;i<datasetInstances.size();i++) {
+                System.out.println("Actual:" + datasetInstances.get(i).classValue());
+                System.out.println("eval:" + evaluation.evaluateModelOnce(j48Classifier, datasetInstances.get(i)));
+            }
+        }
+
+        // Catch block to handle the exceptions
+        catch (Exception e) {
+
+            // Print message on the console
+            System.out.println("Error Occurred!!!! \n"
+                    + e.getMessage());
+        }
+    }
+}
